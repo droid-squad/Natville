a35ca30fc13e4de68292724116ebb7798d06e6cf
Jadal Williams <jwill21@fb.com>
2018-07-16 13:31:57 -0700

    login screen

---
+++ app/src/main/java/me/jwill2385/natville/loginActivity.java
@@ -0,0 +1,22 @@
+package me.jwill2385.natville;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    // when user clicks on login button they will be directed to main screen
+    public void loginToMainScreen(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
