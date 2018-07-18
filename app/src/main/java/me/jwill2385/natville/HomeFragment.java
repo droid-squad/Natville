3f2247c0474b793f3ff1ae574a368268c6ac23a7
Jade N Talley <jntalley@fb.com>
2018-07-18 11:53:38 -0700

    updates to implement google maps

--- app/src/main/java/me/jwill2385/natville/HomeFragment.java
+++ app/src/main/java/me/jwill2385/natville/HomeFragment.java
@@ -1,24 +1,33 @@
 package me.jwill2385.natville;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link HomeFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+///**
+// * A simple {@link Fragment} subclass.
+// * Activities that contain this fragment must implement the
+// * {@link HomeFragment.OnFragmentInteractionListener} interface
+// * to handle interaction events.
+// * Use the {@link HomeFragment#newInstance} factory method to
+// * create an instance of this fragment.
+// */
+
 public class HomeFragment extends Fragment {
+    private static final String TAG = "HomeFragment";
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,10 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    public void init(){
+
     }
 
 
