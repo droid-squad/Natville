c6d7a581aeee3abee1ba933e431cbc88dff74d21
Jadal Williams <jwill21@fb.com>
2018-07-18 15:17:15 -0700

    Updated build gradle

--- app/src/main/java/me/jwill2385/natville/Models/Place.java
+++ app/src/main/java/me/jwill2385/natville/Models/Place.java
@@ -1,16 +1,29 @@
 package me.jwill2385.natville.Models;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Place {
 
     //initialize variables
     private String name;
     private String location;
-    private String pictureURL;
+    private String pictureSmallURL;
+    private String pictureLargeURL;
     private String summary;
     private String distance;
     private double latitutude;
     private double longitude;
 
+    //define variables from JSON object
+    public Place(JSONObject object) throws JSONException{
+        name = object.getString("name");
+        location = object.getString("location");
+        pictureSmallURL = object.getString("imgSqSmall");
+        pictureLargeURL = object.getString("imgSmallMed");
+        summary = object.getString("summary");
+
+    }
 
     //create getter method such that other activities can access these variables
     public String getName() {
@@ -21,8 +34,12 @@ public class Place {
         return location;
     }
 
-    public String getPictureURL() {
-        return pictureURL;
+    public String getPictureSmallURL() {
+        return pictureSmallURL;
+    }
+
+    public String getPictureLargeURL() {
+        return pictureLargeURL;
     }
 
     public String getSummary() {
