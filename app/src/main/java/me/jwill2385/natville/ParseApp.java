0294ece57757259f04fdfee5603a3508ba8b965d
Sol Cruz <solacruz@fb.com>
2018-07-20 15:19:14 -0700

    created ParseApp class and User class

--- app/src/main/java/me/jwill2385/natville/ParseApp.java
+++ app/src/main/java/me/jwill2385/natville/ParseApp.java
@@ -5,19 +5,19 @@ import android.app.Application;
 import com.parse.Parse;
 import com.parse.ParseObject;
 
+
 import me.jwill2385.natville.Models.LocationMap;
 import me.jwill2385.natville.Models.User;
 
 public class ParseApp extends Application{
-
     @Override
     public void onCreate() {
         super.onCreate();
-
+        
         ParseObject.registerSubclass(User.class);
-        ParseObject.registerSubclass(LocationMap.class);
+        ParseObject.registerSubclass(User.class);
 
-        final Parse.Configuration configuration = new Parse.Configuration.Builder(this)
+        final Parse.Configuration configuration=new Parse.Configuration.Builder(this)
                 .applicationId("SEA_park")
                 .clientKey("seattleNature5")
                 .server("http://parks-app.herokuapp.com/parse")
