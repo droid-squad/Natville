fedcc2572c77c7c2d7c9e859d051246910004491
Jade N Talley <jntalley@fb.com>
2018-07-27 14:38:23 -0700

    UI updates for demo

--- app/src/main/java/me/jwill2385/natville/loginActivity.java
+++ app/src/main/java/me/jwill2385/natville/loginActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class loginActivity extends AppCompatActivity {
+    private EditText etUsername;
+    private EditText etPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,10 @@ public class loginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 //        ActionBar actionBar = getSupportActionBar();
 //        actionBar.hide();
+        etUsername = (EditText) findViewById(R.id.etUserName);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etUsername.setSingleLine();
+        etPassword.setSingleLine();
     }
 
     // when user clicks on login button they will be directed to main screen
