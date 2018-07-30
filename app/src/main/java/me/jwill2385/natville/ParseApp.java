894bfac8268c7cd0d190888dafbbcde1e285284c
Sol Cruz <solacruz@fb.com>
2018-07-30 10:36:22 -0700

    merge conflict

--- app/src/main/java/me/jwill2385/natville/ParseApp.java
+++ app/src/main/java/me/jwill2385/natville/ParseApp.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import com.parse.Parse;
 import com.parse.ParseObject;
 
-
 import me.jwill2385.natville.Models.LocationMap;
 import me.jwill2385.natville.Models.User;
 
@@ -14,11 +13,10 @@ public class ParseApp extends Application{
     public void onCreate() {
         super.onCreate();
 
-
         ParseObject.registerSubclass(LocationMap.class);
         ParseObject.registerSubclass(User.class);
 
-        final Parse.Configuration configuration=new Parse.Configuration.Builder(this)
+        final Parse.Configuration configuration = new Parse.Configuration.Builder(this)
                 .applicationId("SEA_park")
                 .clientKey("seattleNature5")
                 .server("http://parks-app.herokuapp.com/parse")
