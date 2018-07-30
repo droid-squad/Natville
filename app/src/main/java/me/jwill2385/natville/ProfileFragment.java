53d0e90bea2fa5a5752a7db39e50f9f957dd3e43
Sol Cruz <solacruz@fb.com>
2018-07-30 16:38:11 -0700

    added user log out ability in the profile fragment page

--- app/src/main/java/me/jwill2385/natville/ProfileFragment.java
+++ app/src/main/java/me/jwill2385/natville/ProfileFragment.java
@@ -40,5 +40,24 @@ public class ProfileFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        logOut = view.findViewById(R.id.bvLogout);
+
+        logOut.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+
+                Intent intent = new Intent(getActivity(), loginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 
 }
